55e8e3ec14f7006b7e0175397bd339120fa4d0c1
naya2matter <https://github.com/naya2matter>
2026-03-30 07:41:04 -0400

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="text-7xl font-bold text-green-400">Hello, World!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
